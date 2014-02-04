f88486fed2891e6954fd6186d9d95036fd8001df
JoeBruner <jbruner@joes-mac.home>
2014-02-03 21:20:31 -0500

    Added vim to the provisioner.  Changed the repo that Accumulo is downloaded from as the old one was sooooo slow.  Reduced the amount of memory allocated to the VM

--- Vagrantfile
+++ Vagrantfile
@@ -10,7 +10,7 @@ Vagrant.configure("2") do |config|
     end
     master.vm.provider :virtualbox do |v|
       v.name = "accumulo-dev-box"
-      v.customize ["modifyvm", :id, "--memory", "6144"]
+      v.customize ["modifyvm", :id, "--memory", "4096"]
     end
     master.vm.network :private_network, ip: "10.211.55.100"
     master.vm.hostname = "accumulo-dev-box"
--- provisioning.sh
+++ provisioning.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+ACCUMULO_VERSION=1.4.4
+
 echo "Updating the system and installing curl and python-software properties..."
 sudo sed -i 's/us.archive.ubuntu.com/mirror.umd.edu/' /etc/apt/sources.list
-sudo apt-get -q update
-sudo apt-get -q install curl python-software-properties -y
+sudo apt-get update
+sudo apt-get install curl python-software-properties vim -y
 
 echo "Installing Sun Java..."
 sudo add-apt-repository ppa:webupd8team/java
@@ -14,20 +16,17 @@ echo debconf shared/accepted-oracle-license-v1-1 seen true | \
   sudo /usr/bin/debconf-set-selections
 sudo apt-get install -y oracle-java7-installer
 
-echo "Running updates..."
-
-
 echo "Setting up environment..."
 cat >> /home/vagrant/.bashrc <<EOF
 export JAVA_HOME=/usr/lib/jvm/java-7-oracle/
 export HADOOP_HOME=/home/vagrant/hadoop-0.20.2-cdh3u3
 export ZOOKEEPER_HOME=/home/vagrant/zookeeper-3.3.4-cdh3u3
-export PATH=$PATH:/home/vagrant/hadoop-0.20.2-cdh3u3/bin:/home/vagrant/accumulo-1.4.3/bin
+export PATH=$PATH:/home/vagrant/hadoop-0.20.2-cdh3u3/bin:/home/vagrant/accumulo-$ACCUMULO_VERSION/bin
 EOF
 export JAVA_HOME=/usr/lib/jvm/java-7-oracle/
 export HADOOP_HOME=/home/vagrant/hadoop-0.20.2-cdh3u3
 export ZOOKEEPER_HOME=/home/vagrant/zookeeper-3.3.4-cdh3u3
-export PATH=$PATH:/home/vagrant/hadoop-0.20.2-cdh3u3/bin:/home/vagrant/accumulo-1.4.3/bin
+export PATH=$PATH:/home/vagrant/hadoop-0.20.2-cdh3u3/bin:/home/vagrant/accumulo-$ACCUMULO_VERSION/bin
 
 echo "Acquiring archives..."
 cd /home/vagrant
@@ -36,12 +35,12 @@ curl -O -L http://archive.cloudera.com/cdh/3/hadoop-0.20.2-cdh3u3.tar.gz
 echo "- Zookeeper"
 curl -O -L http://archive.cloudera.com/cdh/3/zookeeper-3.3.4-cdh3u3.tar.gz
 echo "- Accumulo"
-curl -O -L http://www.gtlib.gatech.edu/pub/apache/accumulo/1.4.4/accumulo-1.4.4-dist.tar.gz
+curl -O -L http://mirrors.gigenet.com/apache/accumulo/$ACCUMULO_VERSION/accumulo-$ACCUMULO_VERSION-dist.tar.gz 
 
 echo "Extracting archives..."
 tar -zxf hadoop-0.20.2-cdh3u3.tar.gz
 tar -zxf zookeeper-3.3.4-cdh3u3.tar.gz
-tar -zxf accumulo-1.4.4-dist.tar.gz
+tar -zxf accumulo-$ACCUMULO_VERSION-dist.tar.gz
 
 echo "Configuring Hadoop..."
 ssh-keygen -t rsa -f /home/vagrant/.ssh/id_rsa -N ''
@@ -112,31 +111,31 @@ echo "Running Zookeeper..."
 zookeeper-3.3.4-cdh3u3/bin/zkServer.sh start
 
 echo "Configuring Accumulo..."
-cp accumulo-1.4.4/conf/examples/1GB/standalone/* accumulo-1.4.4/conf/
+cp accumulo-$ACCUMULO_VERSION/conf/examples/1GB/standalone/* accumulo-$ACCUMULO_VERSION/conf/
 
-cat > ~/accumulo-1.4.4/conf/masters <<EOF
+cat > ~/accumulo-$ACCUMULO_VERSION/conf/masters <<EOF
 accumulo-dev-box
 EOF
 
-cat > ~/accumulo-1.4.4/conf/slaves <<EOF
+cat > ~/accumulo-$ACCUMULO_VERSION/conf/slaves <<EOF
 accumulo-dev-box
 EOF
 
-sed -i 's/>DEFAULT</>password</' accumulo-1.4.4/conf/accumulo-site.xml
-accumulo-1.4.4/bin/accumulo init --clear-instance-name <<EOF
+sed -i 's/>DEFAULT</>password</' accumulo-$ACCUMULO_VERSION/conf/accumulo-site.xml
+accumulo-$ACCUMULO_VERSION/bin/accumulo init --clear-instance-name <<EOF
 accumulo
 password
 password
 EOF
 
 echo "Deploying the Amino Accumulo iterator"
-cp /vagrant/Amino/amino-accumulo-iterators* accumulo-1.4.4/lib/ext/
+cp /vagrant/Amino/amino-accumulo-iterators* accumulo-$ACCUMULO_VERSION/lib/ext/
 
 echo "Starting Accumulo..."
-accumulo-1.4.4/bin/start-all.sh
+accumulo-$ACCUMULO_VERSION/bin/start-all.sh
 
 echo "Adding authorizations"
-accumulo-1.4.4/bin/accumulo shell -u root -p password -e "setauths -s U"
+accumulo-$ACCUMULO_VERSION/bin/accumulo shell -u root -p password -e "setauths -s U"
 
 echo "Creating the HDFS structures for the number example"
 hadoop fs -mkdir /amino/numbers/config
@@ -147,8 +146,4 @@ hadoop fs -copyFromLocal /vagrant/Amino/NumberLoader.xml /amino/numbers/config
 hadoop fs -copyFromLocal /vagrant/Amino/AminoDefaults.xml /amino/numbers/config
 hadoop fs -copyFromLocal /vagrant/Amino/numbers-* /amino/numbers/in
 
-
 echo 'Done!'
-
-
-

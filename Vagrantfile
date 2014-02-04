e302f4389ce1473b0512666e816e40262ab36356
JoeBruner <jbruner@joes-mac.home>
2014-02-04 13:45:13 -0500

    Changed the memory amount back.  Added the code to get Tomcat 7 up and running with BasicAuth for Amino

--- Vagrantfile
+++ Vagrantfile
@@ -10,7 +10,7 @@ Vagrant.configure("2") do |config|
     end
     master.vm.provider :virtualbox do |v|
       v.name = "accumulo-dev-box"
-      v.customize ["modifyvm", :id, "--memory", "4096"]
+      v.customize ["modifyvm", :id, "--memory", "6144"]
     end
     master.vm.network :private_network, ip: "10.211.55.100"
     master.vm.hostname = "accumulo-dev-box"
@@ -18,7 +18,6 @@ Vagrant.configure("2") do |config|
     master.vm.provision :shell, :path=> 'tomcat.sh'
     master.vm.network "forwarded_port", guest: 8080, host: 20426
   end
-
 
 end
 
--- tomcat.sh
+++ tomcat.sh
@@ -5,14 +5,19 @@ export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
 echo "Installing Tomcat7 and admin console"
 sudo apt-get install -y tomcat7 tomcat7-admin
 
+echo "Adding the user accounts to Tomcat"
+sudo sed -i "s/<\/tomcat-users>//" /etc/tomcat7/tomcat-users.xml
+sudo cat >> /etc/tomcat7/tomcat-users.xml <<EOF
+<role rolename="testrole"/>
+<user username="testuser1" password="password" roles="testrole"/>
+<user username="testuser2" password="password" roles="testrole"/>
+<user username="testuser3" password="password" roles="testrole"/>
+<user username="testuser4" password="password" roles="testrole"/>
+<user username="testuser5" password="password" roles="testrole"/>
+<user username="admin" password="password" roles="admin-gui,manager-gui"/>
+</tomcat-users>
+EOF
+
 sudo chgrp -R tomcat7 /etc/tomcat7
 sudo chmod -R g+w /etc/tomcat7 
 
-# https://help.ubuntu.com/13.10/serverguide/tomcat.html
-#The first one is the manager webapp, which you can access by default at http://yourserver:8080/manager/html. It is primarily used to get server status and restart webapps.
-
-#Access to the manager application is protected by default: you need to define a user with the role "manager-gui" in /etc/tomcat7/tomcat-users.xml before you can access it.
-
-#The second one is the host-manager webapp, which you can access by default at http://yourserver:8080/host-manager/html. It can be used to create virtual hosts dynamically.
-
-#Access to the host-manager application is also protected by default: you need to define a user with the role "admin-gui" in /etc/tomcat7/tomcat-users.xml before you can access it.
